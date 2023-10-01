0e3bd4b0ba3de7bd8d2ce713649f23e655138a61
Rene S <axw6@txstate.edu>
2023-09-30 22:43:30 -0500

    Revert "Fix the code. Merge didnt not work."
    This reverts commit eaf19bb258bf623ae5b5e34247df49f1b2be30d9.

--- app/src/main/java/bookstoreproject/pricing/Pricing.java
+++ app/src/main/java/bookstoreproject/pricing/Pricing.java
@@ -1,4 +1,26 @@
 package bookstoreproject.pricing;
+// public class Pricing {
+
+//     private double bookPrice = 10.0;
+//     private double stationaryPrice = 2.0;
+//     private double pencilPrice = 1.0;
+//     private double penPrice = 1.5;
+
+//     public double getPrice(String productType) {
+//         switch (productType) {
+//             case "Book":
+//                 return bookPrice;
+//             case "Stationary":
+//                 return stationaryPrice;
+//             case "Pencil":
+//                 return pencilPrice;
+//             case "Pen":
+//                 return penPrice;
+//             default:
+//                 return 0.0;
+//         }
+//     }
+// }
 
 public abstract class Pricing {
          private double price;
@@ -9,3 +31,25 @@ public abstract class Pricing {
             return (price);
         }
 }
+
+//  public class Book extends Pricing{
+//     // public double price;
+//     public Book(){
+//         super(10.0);
+//     }
+// }
+// public class Stationary extends Pricing{
+//     public Stationary(){
+//         super(2.0);
+//     }
+// }
+// public class Pencil extends Pricing{
+//     public Pencil(){
+//         super(1.0);
+//     }
+// }
+// public class Pen extends Pricing{
+//     public Pen(){
+//         super(1.5);
+//     }
+// }
